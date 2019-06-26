43be2f98f550fc09933508e2e66a9f16cf28bc38
José Antonio Murillo <josemurillo@fb.com>
2019-06-26 11:12:11 -0700

    Initial Commit

---
+++ Flix/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Flix
+//
+//  Created by josemurillo on 6/26/19.
+//  Copyright © 2019 josemurillo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Flix/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  Flix
+//
+//  Created by josemurillo on 6/26/19.
+//  Copyright © 2019 josemurillo. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
